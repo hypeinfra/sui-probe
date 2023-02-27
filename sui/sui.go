9b15a162a476ae0571218d4e66f0b5cf15180e9e
Mysh! <me@mysh.dev>
2023-02-27 17:15:15 +0300

    Add support for basic metrics

--- sui/sui.go
+++ sui/sui.go
@@ -8,11 +8,19 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
 	"time"
 )
 
 const OfficialDevNode = "fullnode.devnet.sui.io"
 const OfficialTestNode = "fullnode.testnet.sui.io"
+
+type MetricsClient struct {
+	Address string
+	Client  *http.Client
+	Header  *http.Header
+	Text    []string
+}
 
 type NodeClient struct {
 	Address string
@@ -118,4 +126,81 @@ func (n *NodeClient) Discover() (json.RawMessage, error) {
 		return nil, err
 	}
 	return values, nil
+}
+
+func NewMetricsClient(address string) *MetricsClient {
+	return &MetricsClient{
+		Address: address,
+		Client: &http.Client{
+			Timeout: 10 * time.Second,
+		},
+		Header: &http.Header{
+			"Content-Type": []string{"text/plain"},
+			"User-Agent":   []string{"SUI-HealthCheck/0.0.4 (+https://github.com/hypeinfra/sui-probe)"},
+		},
+	}
+}
+
+// GetMetrics fetches the metrics from the node
+//
+// Note that it stores the metrics in the Text field,
+// and you need to refresh it when you need to update it
+func (m *MetricsClient) GetMetrics() error {
+	parsedURL, err := url.Parse(m.Address)
+	if err != nil {
+		return err
+	}
+	parsedURL.Path = "/metrics"
+	do, err := m.Client.Do(&http.Request{
+		Method: "GET",
+		URL:    parsedURL,
+		Header: *m.Header,
+	})
+	if err != nil {
+		return err
+	}
+	if do.StatusCode != http.StatusOK {
+		return fmt.Errorf("node's metrics service responded with HTTP status %d", do.StatusCode)
+	}
+	var text []byte
+	text, err = io.ReadAll(do.Body)
+	if err != nil {
+		return err
+	}
+	m.Text = strings.Split(string(text), "\n")
+	return nil
+}
+
+// GetUptime returns the uptime of the node in seconds
+//
+// It checks for "uptime" string in the metrics, and returns the value of the "value" key.
+func (m *MetricsClient) GetUptime() (string, error) {
+	for _, line := range m.Text {
+		if len(line) > 7 {
+			if line[0:7] == "uptime{" {
+				// In the version 0.27.0 this line looks like this:
+				// uptime{version="0.27.0-commit"} 123
+				//
+				// We can get the time by splitting the line by " and getting the second element
+				return strings.Split(line, "\"")[2][2:], nil
+			}
+		}
+	}
+	return "", fmt.Errorf("could not find uptime metric")
+}
+
+// GetPeers returns the number of peers the node is connected to
+//
+// It checks for "sui_network_peers" string in the metrics, and returns the value of the "value" key.
+func (m *MetricsClient) GetPeers() (uint64, error) {
+	for _, line := range m.Text {
+		if len(line) > 17 {
+			if line[0:17] == "sui_network_peers" {
+				// In the version 0.27.0 this line looks like this:
+				// sui_network_peers 4
+				return strconv.ParseUint(strings.Split(line, " ")[1], 10, 64)
+			}
+		}
+	}
+	return 0, fmt.Errorf("could not find peers metric")
 }
